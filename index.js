80b3f92f36f669e04f80980395b1c51d9a9f5d8a
Anush Kumar <17anushkumar@gmail.com>
2024-07-03 13:14:51 +0530

    Updated code with new feature

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-// import express from 'express' -- Warning: To load an ES module, set "type": "module" in the package.json or use the .mjs extension.
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv= require('dotenv')
-const authRoute = require('./routes/auth')
-const userRoute = require('./routes/users')
-const addRoute = require('./routes/adds')
-const commentRoute = require('./routes/comments')
-const cookieParser = require('cookie-parser')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+const addRoute = require('./routes/adds');
+const commentRoute = require('./routes/comments');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const app = express();
 
-const connectDB = async() => {
+const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log("DB 'Adds' is connected.")
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("DB 'Adds' is connected.");
     } catch (error) {
-        console.log("Error in index.js file",error)
+        console.log("Error in index.js file", error);
     }
-}
+};
 
 // Middlewares
-dotenv.config()
-app.use(express.json())
-app.use(cookieParser())
-app.use("/api/auth",authRoute) // this makes the backend api route for auth
-app.use("/api/users",userRoute)
-app.use("/api/adds",addRoute)
-app.use("/api/comments",commentRoute)
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ 
+    origin: "http://localhost:3000", 
+    credentials: true 
+}));
 
-app.listen(process.env.PORT ,() => {
-    connectDB()
-    console.log("App is running.")
-})
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/adds", addRoute);
+app.use("/api/comments", commentRoute);
+
+app.listen(process.env.PORT, () => {
+    connectDB();
+    console.log("App is running.");
+});
